91f28219e06a12c1cb707dd099d5a754e4f1bde4
Alvarobyui <u701860138@byui.edu>
2023-06-30 13:04:59 -0500

    components and images added

---
+++ app/components/Hightlights.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Hightlights() {
+  return (
+    <div>Hightlights</div>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Head from "./components/Head"
+import Main from "./components/Main"
+import Hightlights from './components/Hightlights'
+import Navbar from './components/Navbar'
+
+export default function Home() {
+  return (
+    <body>
+      <div className="container">
+         <Head />
+         <Navbar />
+         <Main />
+         <h1>Today&apos;s Hightlights</h1>
+         <Hightlights />
+      </div>  
+    </body>
+  )
+}
