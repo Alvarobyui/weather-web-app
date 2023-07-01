a2d9ce136923b60ca1aad5b89994858871326989
Alvarobyui <u701860138@byui.edu>
2023-07-01 01:15:05 -0500

    first steps with Main component

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,8 +1,70 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import "./Main.css"
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div id="main">
+        <div className="UIM">
+          <div className="celcious">
+            °C
+          </div>
+          <div className="fahren">
+            °F
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Tomorrow</p>
+            <div className="img">
+              <img src="/sources/Sleet.png" alt="image" />
+            </div>
+            <div className="box">
+              <p>16<span>°C</span></p>
+              <p>11<span>°C</span></p>
+            </div>
+          </div>
+          <div className="card">
+            <p>Tomorrow</p>
+            <div className="img">
+              <img src="/sources/Sleet.png" alt="image" />
+            </div>
+            <div className="box">
+              <p>16<span>°C</span></p>
+              <p>11<span>°C</span></p>
+            </div>
+          </div>
+          <div className="card">
+            <p>Tomorrow</p>
+            <div className="img">
+              <img src="/sources/Sleet.png" alt="image" />
+            </div>
+            <div className="box">
+              <p>16<span>°C</span></p>
+              <p>11<span>°C</span></p>
+            </div>
+          </div>
+          <div className="card">
+            <p>Tomorrow</p>
+            <div className="img">
+              <img src="/sources/Sleet.png" alt="image" />
+            </div>
+            <div className="box">
+              <p>16<span>°C</span></p>
+              <p>11<span>°C</span></p>
+            </div>
+          </div>
+          <div className="card">
+            <p>Tomorrow</p>
+            <div className="img">
+              <img src="/sources/Sleet.png" alt="image" />
+            </div>
+            <div className="box">
+              <p>16<span>°C</span></p>
+              <p>11<span>°C</span></p>
+            </div>
+          </div>
+        </div>
+    </div> 
   )
 }
