d8cd004a38936c38141db0166311ed9644072234
Alvarobyui <u701860138@byui.edu>
2023-06-30 20:40:47 -0500

    navbar added

--- app/components/Head.jsx
+++ app/components/Head.jsx
@@ -1,40 +1,53 @@
+
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import "./Head.css";
+import { useState } from "react";
+import Navbar from "./Navbar";
 
 export default function Head() {
+  const [navbar, setNavbar] = useState(false);
+
+  function handleNavbar() {
+    setNavbar(!navbar);
+  }
   return (
-    <div id="head">
-      <div className="buttons">
-        <button>Search for places</button>
-        <button className="current-location">
-          <img src="sources/currentLocation.png" alt="icon" />
-        </button>
-      </div>
-      <div className="img-box">
-        <div className="img">
-          <img src="sources/shower.png" alt="main-logo" />
+    <>
+      {navbar ? (
+        <Navbar />
+      ) : ("")}
+      <div id="head">
+        <div className="buttons">
+          <button onClick={handleNavbar}>Search for places</button>
+          <button className="current-location">
+            <img src="sources/currentLocation.png" alt="icon" />
+          </button>
+        </div>
+        <div className="img-box">
+          <div className="img">
+            <img src="sources/shower.png" alt="main-logo" />
+          </div>
+        </div>
+        <div className="text">
+          <h1 className="temp">15<span id="celcious">°C</span></h1>
+          <h2 className="descri">Shower</h2>
+          <div className="date">Today - Fri 5 Jun</div>
+          <div className="city">
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="17"
+                height="17"
+                fill="currentColor"
+                className="bi bi-geo-alt-fill"
+                viewBox="0 0 16 16"
+              > <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+              </svg>
+            </span>
+            <p className="city"> Helsinki</p>
+          </div>
         </div>
       </div>
-      <div className="text">
-        <h1 className="temp">15<span id="celcious">°C</span></h1>
-        <h2 className="descri">Shower</h2>
-        <div className="date">Today - Fri 5 Jun</div>
-        <div className="city">
-          <span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="17"
-              height="17"
-              fill="currentColor"
-              className="bi bi-geo-alt-fill"
-              viewBox="0 0 16 16"
-            > <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
-            </svg>
-          </span>
-          <p className="city"> Helsinki</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+import React, { useState } from 'react'
 import "./Navbar.css"
 
 export default function Navbar() {
+  /* const [navbar2, setNavbar2] = useState(true);
+  
+  function handleClick() {
+    setNavbar2(!navbar2); 
+  }*/
    return (
-     <div>Navbar</div>
+     <div>
+        <nav>
+          <div>
+            X
+          </div>
+          <div className="search">
+            <div className="search-box">
+              <span>lupa</span>
+              <input type="text" placeholder='search location'/>
+            </div>
+            <button id='search'>Search</button>
+          </div>
+          <div id="cities">
+            <div id='cities-list'>
+              <a href="#" className="p-box">
+                <p>London</p>
+                <img src="/sources/arrow.png" alt="arrow" />
+              </a>
+              <a href="#" className="p-box">
+                <p>Barcelona</p>
+                <img src="/sources/arrow.png" alt="arrow" />
+              </a>
+              <a href="#" className="p-box">
+                <p>Long Beach</p>
+                <img src="/sources/arrow.png" alt="arrow" />
+              </a>
+            </div>
+          </div>
+        </nav>
+      
+     </div>
    )
 }
  
