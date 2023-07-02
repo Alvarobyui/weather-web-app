32132539d76362f546b24e960bd59ac07474788d
Alvarobyui <u701860138@byui.edu>
2023-07-01 23:20:30 -0500

    Main component done

--- app/components/Hightlights.jsx
+++ app/components/Hightlights.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-import "./Hightlights.css"
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import "./Hightlights.css";
+import { BiSolidNavigation } from "react-icons/bi";
 
 export default function Hightlights() {
   return (
-    <div>Hightlights</div>
-  )
+    <div id="hight">
+      <div className="cards">
+        <div className="card">
+          <p className="wind-text">Wind status</p>
+          <p>
+            7<span className="UM">mph</span>
+          </p>
+          <div className="wind-info">
+            <div className="wind-arrow">
+              <BiSolidNavigation />
+            </div>
+            <p>WSW</p>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <p>created by Alvaro Diaz - Funval</p>
+      </footer>
+    </div>
+  );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,20 @@
-"use client"
-import Head from "./components/Head"
-import Main from "./components/Main"
-import Hightlights from './components/Hightlights'
-import Navbar from './components/Navbar'
+"use client";
+import Head from "./components/Head";
+import Main from "./components/Main";
+import Hightlights from "./components/Hightlights";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <body>
       <div className="container">
-         <Head/>
-         <Main />
-         <h1>Today&apos;s Hightlights</h1>
-         <Hightlights />
-      </div>  
+        <Head />
+        <div className="content">
+          <Main />
+          <h1>Today&apos;s Hightlights</h1>
+          <Hightlights />
+        </div>
+      </div>
     </body>
-  )
+  );
 }
